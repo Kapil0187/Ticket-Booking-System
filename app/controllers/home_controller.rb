13a4aa36afc1c9a0c658cb1a6e1edc1f10210401
kapil <kapilbarsker2003@gmail.com>
2024-08-02 14:35:29 +0530

    Variable type changed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,10 @@
 class HomeController < ApplicationController
-  def index
-    @directions = Direction.all
-    @buses = Bus.all  
+  def index 
   end
 
   def search
-    @direction = Direction.where(source:params[:source],destination:params[:destination]).pluck(:id)
-    @buses = Bus.where("direction_id = ?",@direction)
+    direction = Direction.where(source:params[:source],destination:params[:destination]).pluck(:id)
+    @buses = Bus.where("direction_id = ?",direction)
   end
 
 end
