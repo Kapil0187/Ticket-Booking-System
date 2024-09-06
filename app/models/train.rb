e15241f2e14175ec554f6c1a142ef13d3b26149e
Kapil0187 <90748985+Kapil0187@users.noreply.github.com>
2024-09-06 11:17:15 +0530

    Update train.rb

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,11 +1,5 @@
 class Train < ApplicationRecord
-  validates :name, presence: true
-  validates :total_seets, presence: true
-  validates :departure_time, presence: true
-  validates :arrival_time, presence: true
-  validates :price, presence: true
-  validates :direction_id, presence: true
-  validates :traintype, presence: true
+  validates :name, :total_seets, :departure_time, :arrival_time, :price, :direction_id, :traintype, presence: true
 
   belongs_to :direction
   has_many :bookings, as: :bookingable
