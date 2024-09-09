12e5c980e301468488aff1cec4eb72fd2c985c75
kapil <kapilbarsker2003@gmail.com>
2024-09-09 16:44:09 +0530

    changed in trains_spec file

--- spec/requests/trains_spec.rb
+++ spec/requests/trains_spec.rb
@@ -1,14 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe 'Trains', type: :request do
-#   context 'Get /show_train' do
-#     it 'should render show train page' do
-#       get train_path
-#       expect(response).to render_template :show_train
-#     end
-#   end
-# end
-# spec/controllers/trains_controller_spec.rb
 require 'rails_helper'
 
 RSpec.describe TrainsController, type: :controller do
