447d2e8815c08264220bdddca7117e915bdca599
kapil <kapilbarsker2003@gmail.com>
2024-09-09 16:24:45 +0530

    Wrote rspec for flights controller

--- spec/requests/flights_spec.rb
+++ spec/requests/flights_spec.rb
@@ -1,10 +1,44 @@
+# spec/controllers/flights_controller_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'Flights', type: :request do
-  describe 'GET /index' do
-    it 'works! (now write some real specs)' do
-      get flight_path
-      expect(response).to have_http_status(200)
+RSpec.describe FlightsController, type: :controller do
+  describe 'GET #index' do
+    it 'renders the index template' do
+      get :index
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show_flight' do
+    let!(:direction) { Direction.create!(source: 'New York', destination: 'india', distance: 10_000) }
+    let!(:flight) do
+      Flight.create!(direction_id: direction.id, name: 'Air India', total_seets: 200, departure_time: Time.now,
+                     arrival_time: Time.now, price: 20_000)
+    end
+
+    context 'when valid parameters are passed' do
+      before do
+        get :show_flight, params: { source: 'New York', destination: 'india' }
+      end
+
+      it 'assigns @flights' do
+        expect(assigns(:flights)).to eq([flight])
+      end
+
+      it 'renders the show_flight template' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when no matching direction is found' do
+      before do
+        get :show_flight, params: { source: 'india', destination: 'New York' }
+      end
+
+      it 'renders the show_flight template' do
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
